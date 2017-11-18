c10d465984ea9bc03982aca4c380392fee5c8d44
Almir Dapper <Almir Dapper@AlmirDapper>
2017-11-18 17:19:29 -0200

    Projeto  18-11-2017 scrip banco na pasta ok

---
+++ Bancobiblioteca mysql.sql
@@ -0,0 +1,29 @@
+create database exercicio2Biblioteca;
+
+use exercicio2Biblioteca;
+
+create table login(
+login varchar(10),
+senha varchar(10)
+);
+
+create table livros(
+id int primary key auto_increment,
+titulo varchar(30),
+autores varchar(30),
+genero varchar(30),
+ano int,
+disponivel varchar(3)
+);
+
+create table usuarios(
+matricula int primary key auto_increment,
+nome varchar(40),
+login varchar(30),
+senha varchar(10),
+dataNascimento varchar(10)
+);
+
+insert into usuarios (nome, login, senha) values('administrador', 'adm', 'adm');
+
+select * from usuarios;
